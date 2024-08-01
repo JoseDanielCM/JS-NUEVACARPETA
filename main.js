acbd667ef6a32bde2cca68b9623dcbd7f79e50e9
JoseDanielCM <jodacarmon@gmail.com>
2024-08-01 11:22:01 -0500

    feat: :zap: Estilos, y numeracion de pagina

--- main.js
+++ main.js
@@ -2,43 +2,49 @@ let pag = 1
 const btnAntes = document.getElementById("btnAntes")
 const btnDespues = document.getElementById("btnDespues")
 
+
+
+document.getElementById("numeroDePagina").innerHTML=pag 
+
 btnDespues.addEventListener("click",function(){
-    if (pag<1000){
+    if (pag<500){
         pag++
+        document.getElementById("numeroDePagina").innerHTML=pag 
         mostrarPeliculas()
+    }else{
+        alert("ᓚᘏᗢ  - Es la ultima pagina  -  ᓚᘏᗢ")
     }
+    
 })
 
 btnAntes.addEventListener("click",function(){
     if (pag>1){
         pag--
+        document.getElementById("numeroDePagina").innerHTML=pag 
         mostrarPeliculas()
     }
 })
 
 const mostrarPeliculas = async function () {
+    var urlLink = `https://api.themoviedb.org/3/movie/popular?api_key=69776d98d3f8da6dcc01c3d5b234f72b&language=en&page=${pag}`
     try {
-        const respuesta = await fetch(`https://api.themoviedb.org/3/movie/popular?api_key=69776d98d3f8da6dcc01c3d5b234f72b&language=en&page=${pag}`);
+        const respuesta = await fetch(urlLink);
 
         if (!respuesta.ok) {
             throw new Error(`Network response was not ok ` + respuesta.statusText)
         }
         const datos = await respuesta.json()
         listaPeliculas = datos.results
-        console.log(listaPeliculas)
         let peliculas=""
         for (const pelicula of listaPeliculas) {
             peliculas+=`
             <div class="pelicula">
                 <img class="poster" src="https://image.tmdb.org/t/p/w500/${pelicula.poster_path}.jpg">
+                <h3 class="titulo">${pelicula.title}</h3>
             </div>
-            <h1>${pelicula.title}</h1>
             `;
         }
         document.getElementById("contenedor").innerHTML = peliculas
-
-
-
 
     } catch (error) {
         console.error("Hubo un problema con la solicitud: " + error)
